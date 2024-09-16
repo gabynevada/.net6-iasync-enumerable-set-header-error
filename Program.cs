47efc3f14bf457f3c6e8f841c1868cba35f01135
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2024-09-16 12:22:37 -0400

    chore: remove unused swagger and add NuGet config

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using SetResponseHeaders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,24 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SetResponseHeaders", Version = "v1" });
-});
-
 builder.Services.AddScoped<IDataService, DataService>();
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (builder.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SetResponseHeaders v1"));
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
