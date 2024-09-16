af8cd3a5caaaf1fcaf8a424aacdb7b81d432bb24
Elvis Nieves <elvis.nievesmiranda@gmail.com>
2024-09-16 11:55:16 -0400

    feat: add cannot set headers error implementation for dotnet 9 (#1)
    * feat: add cannot set headers error implementation for dotnet 9
    
    * Remove unused dependencies

--- IDataService.cs
+++ IDataService.cs
@@ -1,14 +1,8 @@
-
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using SetResponseHeaders.Db;
 
-namespace SetResponseHeaders
+namespace SetResponseHeaders;
+
+public interface IDataService
 {
-    public interface IDataService
-    {
-        Task<WeatherForecast> GetData();
-        IAsyncEnumerable<Data> GetDataFromDb();
-        Task AddData();
-    }
+    Task<WeatherForecast> GetData();
 }
